5c540c7064785f912d5b1eeead9b6383ed2ac16f
Saiyannaing23 <pesilay2372002@gmail.com>
2023-07-21 17:15:12 +0630

    list render without components

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,88 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <notifications group="foo" />
+    <div class="row justify-content-center">
+      <div class="col-11">
+        <img src="./assets/logo.png" alt="Vue-Logo" class="d-block mx-auto">
+        <form @submit.prevent="addItems">
+          <label>New Task</label>
+          <input type="text" class="form-control mt-1" v-model="task">
+          <button class="btn btn-dark mt-3" type="submit">Add New Task</button>
+        </form>
+      </div>
+    </div>
+    <div class="row justify-content-center mt-3">
+      <div class="col-11 mt-3" v-for="(todo,index) in todos" :key="index" >
+        <div class="card">
+          <div class="row p-3 align-items-center">
+            <div class="col-7">
+            <span :class="{done : todo.completed}">{{todo.task}}</span> 
+            </div>
+            <div class="col text-end">
+              <button class="btn btn-success mr-3 btn-c" @click="completeTask(index)"><i class="material-icons mt-1">check</i></button>
+              <button class="btn btn-danger" @click="removeTask(index)"><i class="material-icons mt-1">delete</i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data : ()=>({
+    task : "",
+    todos : [],
+  }),
+  methods :{
+    addItems(){
+      this.todos.push({task:this.task,completed:false});
+      this.task = "";
+    },
+    removeTask(index){
+      this.todos.splice(index,1)
+      this.$notify({
+        group: 'foo',
+        title: 'Deleted!',
+        text: 'Hello user! Your Task has been removed!',
+        type: 'warn',
+      });
+    },
+    completeTask(index){
+      this.todos[index].completed = true;
+      this.$notify({
+        group: 'foo',
+        title: 'Completed!',
+        text: 'Hello user! Your Task is completed!',
+        type: 'success',
+      });
+    },
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+img{
+  width: 130px;
+  margin-top: 10%;
+  margin-bottom: 30px;
+}
+label{
+  color: #fff;
+}
+.btn-c{
+  margin-right: 10px;
+  display: inline-block;
+}
+.card{
+  border-radius: 1rem !important;
+}
+.done{
+  text-decoration: line-through;
+}
+span{
+  font-size: 20px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import "./assets/app.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Notifications from 'vue-notification';
+Vue.use(Notifications)
 Vue.config.productionTip = false
 
 new Vue({
